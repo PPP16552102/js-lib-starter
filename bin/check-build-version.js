3dc84b38f5dc47c1f0ba7bc589e68b1ffce6b966
Peter <2814594602@qq.com>
2025-05-23 11:02:51 +0800

    ci: github ci

--- bin/check-build-version.js
+++ bin/check-build-version.js
@@ -7,20 +7,13 @@ const { version } = JSON.parse(fs.readFileSync('./package.json'));
 console.log('Checking versions...\n----------------------------');
 
 console.log(`Package version: v${version}`);
-console.log(`Axios version: v${app.VERSION}`);
-// console.log(`Axios build version: v${appBuild.VERSION}`);
+console.log(`App version: v${app.VERSION}`);
 console.log(`----------------------------`);
 
 assert.strictEqual(
   version,
   app.VERSION,
-  `Version mismatch between package and Axios ${version} != ${app.VERSION}`,
+  `Version mismatch between package and App ${version} != ${app.VERSION}`,
 );
-
-// assert.strictEqual(
-//   version,
-//   appBuild.VERSION,
-//   `Version mismatch between package and build ${version} != ${appBuild.VERSION}`,
-// );
 
 console.log('✔️ PASSED\n');
